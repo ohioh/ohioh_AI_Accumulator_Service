a0b49891811f5c6f5f4f8e13bc3be345a2c52737
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-02 09:07:08 +0100

    Fixed schema bug

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     MONGO_URI = '{}/{}?retryWrites=true&w=majority'.format(os.getenv('DB_CONNECTION_URL'),os.getenv("DB_NAME"))
 
-    MONGO_1 = '{}'.format(os.getenv("CLUSTER_1"))
-    MONGO_2 = '{}'.format(os.getenv("CLUSTER_2"))
-    MONGO_3 = '{}'.format(os.getenv("CLUSTER_3"))
-    MONGO_4 = '{}'.format(os.getenv("CLUSTER_4"))
+    MONGO_1 = '{}/ohioh?retryWrites=true&w=majority'.format(os.getenv("CLUSTER_1"))
+    MONGO_2 = '{}/ohioh?retryWrites=true&w=majority'.format(os.getenv("CLUSTER_2"))
+    MONGO_3 = '{}/ohioh?retryWrites=true&w=majority'.format(os.getenv("CLUSTER_3"))
+    MONGO_4 = '{}/ohioh?retryWrites=true&w=majority'.format(os.getenv("CLUSTER_4"))
